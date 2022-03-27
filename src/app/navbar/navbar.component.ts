74071b63c72d27dd6a635f8e4a00efd517e5a982
Prem Kumar Yadav <premk378@gmail.com>
2022-03-27 19:56:20 +0530

    changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,19 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  showModal: boolean;
-  modalData: any = {};
   constructor() { }
 
   ngOnInit() {
   }
 
-  showInfo(){
-    this.showModal = true;
-    this.modalData = {type: 'info', title: 'How to Play?', text: 'Rules'};
-  }
-
-  closeModal() {
-    this.showModal = false;
-  }
 }
